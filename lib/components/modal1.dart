0d0fbd6ec7476e36773db6ee0108c7f375b4c62a
saiashish9 <saiashish3760@gmail.com>
2021-09-29 22:34:44 +0530

    small changes

--- lib/components/modal1.dart
+++ lib/components/modal1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'header.dart';
 
 class Modal1 extends StatelessWidget {
@@ -9,6 +11,8 @@ class Modal1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Container(
       padding: EdgeInsets.symmetric(
           horizontal: MediaQuery.of(context).size.width * 0.05),
@@ -17,7 +21,6 @@ class Modal1 extends StatelessWidget {
         children: [
           Container(
               margin: const EdgeInsets.symmetric(vertical: 7),
-              padding: const EdgeInsets.only(right: 18),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -26,6 +29,43 @@ class Modal1 extends StatelessWidget {
                     subTitle:
                         "move the dial and set any amount you need upto ₹487,891",
                   ),
+                  const SizedBox(height: 6),
+                  Container(
+                    height: size.height * 0.54,
+                    width: size.width * 0.9,
+                    padding: EdgeInsets.symmetric(vertical: size.height * 0.04),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(18))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                            height: size.height * 0.36,
+                            child: Image.asset('assets/images/graph.png',
+                                fit: BoxFit.cover)),
+                        Column(
+                          children: const [
+                            Text(
+                              'stash is instant. money will be credited within',
+                              style: TextStyle(
+                                  color: Color(0xFF999999),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'seconds.',
+                              style: TextStyle(
+                                  color: Color(0xFF999999),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               )),
         ],
